18a8c450afc851ffd7d66555b03662ca4cebaaa4
Matthew Burns <mburns@equinix.com>
2025-03-11 12:36:55 -0700

    Added tests

--- internal/racadm/racadm_test.go
+++ internal/racadm/racadm_test.go
@@ -1,1 +1,40 @@
 package racadm
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	ex "github.com/metal-toolbox/bmclib/internal/executor"
+)
+
+func newFakeRacadm(t *testing.T, fixtureName string) *Racadm {
+	e := &Racadm{
+		Executor: ex.NewFakeExecutor("racadm"),
+	}
+
+	b, err := os.ReadFile("../../fixtures/internal/racadm/" + fixtureName)
+	if err != nil {
+		t.Error(err)
+	}
+
+	e.Executor.SetStdout(b)
+
+	return e
+}
+
+func TestExec_Run(t *testing.T) {
+	// Create a new instance of Sum
+	exec := newFakeRacadm(t, "SetBiosConfigFromFile")
+
+	// Create a new context
+	ctx := context.Background()
+
+	// Call the run function
+	_, err := exec.run(ctx, "SetBiosConfigFromFile")
+
+	// Check the output and error
+	if err != nil {
+		t.Errorf("Expected no error, got: %v", err)
+	}
+}
